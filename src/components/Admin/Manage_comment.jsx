d4c97b8710817637b8b754c96cce6aa521500f73
Abdulrahman <abdulrhmanelthrawi@gmail.com>
2026-08-20 16:45:59 +0300

    fix yellow line

--- src/components/Admin/Manage_comment.jsx
+++ src/components/Admin/Manage_comment.jsx
@@ -164,7 +164,7 @@ function Comment() {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-[#f0f4f8]">
+      <div className="min-h-screen flex items-center justify-center ">
         <div className="text-center">
           <div className="animate-spin w-12 h-12 border-4 border-primary border-t-transparent rounded-full mx-auto mb-4"></div>
           <p className="font-bold text-primary">جاري التحقق...</p>
